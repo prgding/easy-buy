83edd2dd37b6c0cb94464addf5ca3370ccd7ad94
dingshuai <1203823603@qq.com>
2022-12-10 13:51:36 +0800

    只剩一个留言回复

--- src/main/java/me/dingshuai/test/ReplyServlet.java
+++ src/main/java/me/dingshuai/test/ReplyServlet.java
@@ -5,6 +5,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import me.dingshuai.dao.TmessagesDao;
+import me.dingshuai.dao.impl.TmessagesDaoImpl;
+import me.dingshuai.pojo.Tmessages;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,6 +19,10 @@ public class ReplyServlet extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		String msgId = request.getParameter("msgId");
+
+		TmessagesDao td = new TmessagesDaoImpl();
+		Tmessages tm = new Tmessages();
+		tm = td.findById(Integer.parseInt(msgId));
 
 	}
 }
