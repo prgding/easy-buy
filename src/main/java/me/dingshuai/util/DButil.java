7ba824cbcb3e72979613391b4f12e24fc83f1f7e
Ding Shuai <1203823603@qq.com>
2023-01-25 11:25:14 +0800

    标准的目录安排，能够把web资源打包进去

--- src/main/java/me/dingshuai/util/DButil.java
+++ src/main/java/me/dingshuai/util/DButil.java
@@ -1,6 +1,9 @@
 package me.dingshuai.util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class DButil {
